dd26cb37774b3161935766427ee44cb82693d31f
babbcb2 <babbcb4@gmail.com>
2021-02-19 10:09:57 +0900

    semiproject update

--- Django/LECTURE/sampleWEB/baseWEB/GreetingApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from GreetingApp import views
-
-urlpatterns = [
-    path('hi/', views.hi),
-]
-
--- Django/LECTURE/sampleWEB/baseWEB/GreetingApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def hi(request) :
-    return HttpResponse ('섭이와 함께하는 장고 웹')
-
